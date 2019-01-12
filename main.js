07676509f9ef880e9f7a4ddcabb46b1a1d6e3327
AleksanderSkubala <olek_s@wtvk.pl>
2019-01-12 21:50:27 +0100

    Add new query, a little bit of script, styles and footer

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 //'onload' data
 
-const category = ['business', 'technology', 'science', 'politics'];
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          "category="+category[3]+"&" +
-          'country=pl&' +
-          'apiKey=7d3aa33090004b94aa83763a7c2730f1';
+var url = 'https://newsapi.org/v2/everything?'+
+           'q=strajk&'+    //searching query
+           'from=2019-01-05&'+   //from date
+           'sortBy=popularity,relevancy&'+    //sortBy
+           'apiKey=7d3aa33090004b94aa83763a7c2730f1';    //apiKey
 
 
 var des = document.querySelector('.description');
 var grid = document.querySelector('.grid');
 
 //getting NewsApi result
+getResponse(url);
 
-axios.get(url)
-  .then(response => accessGranted(response))
-  .catch(error => accessDenied(error));
+function getResponse(surl){
+    axios.get(surl)
+    .then(response => accessGranted(response))
+    .catch(error => accessDenied(error));
+}
 
 function accessDenied(err) {
     console.error(err);
@@ -28,7 +31,8 @@ function accessGranted(result) {
     data.forEach(item => {
         console.log(item.title);
 
-        var title = item.title.substring(0, 45)+'...';
+        var title = '<b>'+item.title.substring(0, 35)+'...</b>';
+        var description = item.description.substring(0, 45)+'...';
         var url = item.url;
         var img = item.urlToImage;
 
@@ -37,11 +41,15 @@ function accessGranted(result) {
         itemParent.classList.add('itemGrid');
         var itemGrid = document.createElement('div');
         itemGrid.classList.add('overlay');
-        itemGrid.innerHTML = title;
+        itemGrid.innerHTML = title+'<br/>'+description;
 
         //setting styles
         itemParent.style.backgroundImage = "url("+img+")";
-        itemParent.style.backgroundPosition = "30% 40%";
+
+        //setting event listeners
+        itemParent.addEventListener("click", ()=>{
+            window.location = url;
+        });
 
         //appending childs
         itemParent.appendChild(itemGrid);
